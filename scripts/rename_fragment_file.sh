8e261d7c55776fed8f906f44fb5d0bb3937315cb
SuvojitHazra <129977598+SuvojitHazra@users.noreply.github.com>
2025-03-21 15:06:43 -0500

    Update rename_fragment_file.sh

--- scripts/rename_fragment_file.sh
+++ scripts/rename_fragment_file.sh
@@ -1,5 +1,5 @@
-zcat filtered_fragments_corrected_dedup_count.tsv.gz | awk '
+zcat results/filtered_fragments_corrected_dedup_count.tsv.gz | awk '
 {
     chr_num = substr($1, 4) + 0  # Extract number and remove leading zeros
     print "chr" chr_num, $2, $3, $4, $5
-}' OFS="\t" | gzip > renamed_filtered_fragments_corrected_dedup_count.tsv.gz
+}' OFS="\t" | gzip > results/renamed_filtered_fragments_corrected_dedup_count.tsv.gz
